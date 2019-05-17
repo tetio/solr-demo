bd6e4d9af0334809837ad008a0af2c79ed272bda
Sergi <meuxilus@gmail.com>
2019-05-17 09:25:35 +0200

    query fixed

--- src/main/java/net/portic/solr/repository/DocumentRepository.java
+++ src/main/java/net/portic/solr/repository/DocumentRepository.java
@@ -17,9 +17,9 @@ public interface DocumentRepository extends SolrCrudRepository<Document, Integer
     @Query("ownerId:?1 AND (booking:?0 OR shipCall:?0 OR equipmentNumbers:?0 OR equipmentRefs:?0)")
     Iterable<Document> findAll(String value, String owner);
 
-    @Query(" updated:[?0 TO ?1]")
+    @Query("updated:[?0 TO ?1]")
     Iterable<Document> findAllDate(String fromDate, String toDate);
 
-    @Query("ownerId:?1 AND updated:[?1 TO ?2] ")
+    @Query("ownerId:?0 AND updated:[?1 TO ?2] ")
     Iterable<Document> findAllDate(String ownerId, String fromDate, String toDate);
 }
