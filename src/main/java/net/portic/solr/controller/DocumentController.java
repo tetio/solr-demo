c2c6dcc728bebf113084722dff39b8357d8fbb89
Sergi <meuxilus@gmail.com>
2019-05-18 12:22:48 +0200

    document generator with faker. More fields are needed.

--- src/main/java/net/portic/solr/controller/DocumentController.java
+++ src/main/java/net/portic/solr/controller/DocumentController.java
@@ -5,9 +5,11 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
+import com.github.javafaker.Faker;
 import net.portic.solr.dto.SearchDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.convert.DateTimeConverters;
@@ -22,9 +24,49 @@ public class DocumentController {
     @Autowired
     private DocumentRepository repository;
 
+    @GetMapping("/document")
+    public Iterable<Document> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/document/{id}")
+    public Document getDocument(@PathVariable Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/document")
+    public Document createDocument(@RequestBody Document document) {
+        return repository.save(document);
+    }
+
+    @PostMapping("/document/finddate")
+    public Iterable<Document> findDocumenDatet(@RequestBody SearchDTO searchDTO) {
+        if (isSuperUser(searchDTO.getOwnerId())) {
+            return repository.findAllDate(searchDTO.getFromDate(), searchDTO.getToDate());
+        }
+        return repository.findAllDate(searchDTO.getOwnerId(), searchDTO.getFromDate(), searchDTO.getToDate());
+    }
+
+    @PostMapping("/document/findcriteria")
+    public Iterable<Document> findDocumentCriteria(@RequestBody SearchDTO searchDTO) {
+        if (isSuperUser(searchDTO.getOwnerId())) {
+            return repository.findAll(searchDTO.getSearchTerm());
+        }
+        return repository.findAll(searchDTO.getSearchTerm(), searchDTO.getOwnerId());
+    }
+
+    private boolean isSuperUser(String ownerId) {
+        return ("A100000001".equalsIgnoreCase(ownerId));
+    }
+
+    @PutMapping("/document")
+    public Document updateDocument(@RequestBody Document document) {
+        return repository.save(document);
+    }
+
+
     @PostConstruct
     public void addDocuments() {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         List<Document> documents = new ArrayList<>();
         LocalDateTime ldtnow = LocalDateTime.now();
         Date now = Date.from(ldtnow.atZone(ZoneId.systemDefault()).toInstant());
@@ -91,45 +133,29 @@ public class DocumentController {
                 .build());
         repository.saveAll(documents);
 
-    }
-
-    @GetMapping("/document")
-    public Iterable<Document> getAll() {
-        return repository.findAll();
-    }
-
-    @GetMapping("/document/{id}")
-    public Document getDocument(@PathVariable Integer id) {
-        return repository.findById(id).orElse(null);
-    }
-
-    @PostMapping("/document")
-    public Document createDocument(@RequestBody Document document) {
-        return repository.save(document);
-    }
-
-    @PostMapping("/document/finddate")
-    public Iterable<Document> findDocumenDatet(@RequestBody SearchDTO searchDTO) {
-        if (isSuperUser(searchDTO.getOwnerId())) {
-            return repository.findAllDate(searchDTO.getFromDate(), searchDTO.getToDate());
+        Faker faker = new Faker();
+        Document document;
+        String ownerId = "QZ7778888";
+        Date when;
+        for (int i = 0; i < 10000; i++) {
+            if (i % 5 == 0) {
+                ownerId = "QA331122111";
+            } else {
+                ownerId = "QZ7778888";
+            }
+            when = faker.date().past(faker.number().numberBetween(1, 1000), TimeUnit.DAYS);
+            document = Document.builder()
+                    .id(faker.idNumber().valid())
+                    .shipCall("" + faker.number().numberBetween(10000, 100000))
+                    .booking(faker.idNumber().valid())
+                    .equipmentNumbers(new String[]{faker.random().hex(3) + "U" + faker.number().digits((7)), faker.random().hex(3) + "U" + faker.number().digits((7))})
+                    .equipmentRefs(new String[]{"REF" + faker.number().digits(4), "REF" + faker.number().digits(4)})
+                    .ownerId(ownerId)
+                    .username(faker.pokemon().name())
+                    .created(when)
+                    .updated(when)
+                    .build();
+            repository.save(document);
         }
-        return repository.findAllDate(searchDTO.getOwnerId(), searchDTO.getFromDate(), searchDTO.getToDate());
-    }
-
-    @PostMapping("/document/findcriteria")
-    public Iterable<Document> findDocumentCriteria(@RequestBody SearchDTO searchDTO) {
-        if (isSuperUser(searchDTO.getOwnerId())) {
-            return repository.findAll(searchDTO.getSearchTerm());
-        }
-        return repository.findAll(searchDTO.getSearchTerm(), searchDTO.getOwnerId());
-    }
-
-    private boolean isSuperUser(String ownerId) {
-        return ("A100000001".equalsIgnoreCase(ownerId));
-    }
-
-    @PutMapping("/document")
-    public Document updateDocument(@RequestBody Document document) {
-        return repository.save(document);
     }
 }
--- src/main/java/net/portic/solr/repository/DocumentRepository.java
+++ src/main/java/net/portic/solr/repository/DocumentRepository.java
@@ -11,7 +11,9 @@ public interface DocumentRepository extends SolrCrudRepository<Document, Integer
     Iterable<Document> findAllByShipCall(String value);
     Iterable<Document> findAllByBooking(String value);
 
-    @Query("booking:?0 OR shipCall:?0 OR equipmentNumbers:?0 OR equipmentRefs:?0")
+//    @Query("booking:?0 OR shipCall:?0 OR equipmentNumbers:?0 OR equipmentRefs:?0")
+//    Iterable<Document> findAll(String value);
+    @Query("booking:?0 OR shipCall:?0 OR equipmentNumbers:?0 OR equipmentRefs:?0 OR username:?0")
     Iterable<Document> findAll(String value);
 
     @Query("ownerId:?1 AND (booking:?0 OR shipCall:?0 OR equipmentNumbers:?0 OR equipmentRefs:?0)")
